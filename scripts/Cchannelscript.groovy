bbbc94d1271dd1ccfd795a941450e98b32e6a321
NauticalTooth27 <ryan@polhemus.org>
2017-01-13 14:23:25 -0500

    Cleaned
    Debug print statements removed, deprecated code removed

--- scripts/Cchannelscript.groovy
+++ scripts/Cchannelscript.groovy
@@ -35,8 +35,6 @@ CSG getSheet(){
 			
   	CSG stub = sheet.difference(hole)
   	CSG outputSheet = stub.clone()
-  	System.out.println("Sucess")
-  	System.out.println(vexSheetConfig.y)
   	if (vexSheetConfig.x != 2)
 	{
 	for (int i=-1;i< vexSheetConfig.x + 1;i++){
@@ -75,7 +73,6 @@ CSG getSheet(){
 	}
 	else
 	{
-		System.out.println("cool")
 		for (int i=-1;i< vexSheetConfig.x + 1;i++){
 			if (i == -1) {
 				for (int j=0;j<vexSheetConfig.y;j++){
@@ -102,9 +99,6 @@ CSG getSheet(){
   					stub
   						.movex(i*side)
   						.movey(j*side)
-
-  		
-  					
   				)
   		}
 			}
@@ -113,81 +107,8 @@ CSG getSheet(){
 			outputSheet = outputSheet.difference(midhole.movey(12.5*x+ 6.0).movex(6.0))
 		}
 	}
-	
-	/*for (int i=0;i< vexSheetConfig.x;i++){
-
-		
-		
-  		for (int j=0;j<vexSheetConfig.y;j++){
-
-  			if (i == 0 || i == vexSheetConfig.x -1)
-  			{
-  				outputSheet.union(stub.movex(-(i*side)).rotz(90))
-  			}
-  			
-  			outputSheet = outputSheet.union(
-  					stub
-  						.movex(i*side)
-  						.movey(j*side))
-				
-  		}
-	}
-
-  */
-			
-  	//outputSheet = outputSheet.union(new Cube(side).toCSG())
 	return outputSheet
 		.setParameter(size)
-		.setRegenerate({getSheet()})
-
-		
+		.setRegenerate({getSheet()})	
 }
-
-/*
-CSG getNut(){
-	String type= "vexFlatSheet"
-	if(args==null)
-		args=["1x5"]
-	StringParameter size = new StringParameter(	type+" Default",
-										args.get(0),
-										Vitamins.listVitaminSizes(type))
-	//println "Database loaded "+database
-	HashMap<String,Object> vexSheetConfig = Vitamins.getConfiguration( type,size.getStrValue())
-
-	double thickness = 0.8
-	double side = 12.5
-
-	double nunRad = ((side/3)/(2*Math.sin(Math.PI/8)))*3/2
-	CSG head =new Cylinder(nunRad,nunRad,thickness,(int)8).toCSG() 
-				.rotz(45/2)
-  
-   	CSG sheet = new Cube(	side,// X dimention
-			side,// Y dimention
-			thickness//  Z dimention
-			).toCSG()// this converts from the geometry to an object we can work with
-			.intersect(head)
-	CSG hole = new Cube(	4.7,// X dimention
-			4.7,// Y dimention
-			thickness*2//  Z dimention
-			).toCSG()// this converts from the geometry to an object we can work with
-			
-  	CSG stub = sheet.difference(hole)
-  	CSG outputSheet = stub.clone()
-  	for (int i=0;i< vexSheetConfig.x;i++){
-  		for (int j=0;j<vexSheetConfig.y;j++){
-  			outputSheet = outputSheet.union(
-  					stub
-  						.movex(i*side)
-  						.movey(j*side)
-  					
-  				)
-  		}
-  	}
-  	//outputSheet = outputSheet.union(new Cube(side).toCSG())
-	return outputSheet
-		.setParameter(size)
-		.setRegenerate({getNut()})
-}
-return getNut()
-*/
 return getSheet()
